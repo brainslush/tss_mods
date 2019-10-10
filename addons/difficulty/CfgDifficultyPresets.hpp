2c600366d11fa31c7a40b1cba4f0cc963b514fb0
brainslush <benny@horrorservice.de>
2019-10-10 18:04:36 +0200

    changed difficulty settings and fixed ai skill issue

--- addons/difficulty/CfgDifficultyPresets.hpp
+++ addons/difficulty/CfgDifficultyPresets.hpp
@@ -3,12 +3,12 @@ class CfgDifficultyPresets {
     delete Regular;
     delete Veteran;
     defaultPreset = QUOTE(ADDON);
-    levelAI = QGVVAR();
     class ADDON {
         description = "Hat das TSS BCT bestanden";
         displayName = "TSS";
         optionDescription = "Hat das TSS BCT bestanden";
         optionPicture = QPATHTOEF(main,data\ui\tss.paa);
+        levelAI = QGVVAR(TSS);
         // aiLevelPreset defines AI skill level and is counted from 0 and can have following values: 0 (Low), 1 (Normal), 2 (High), 3 (Custom).
         // when 3 (Custom) is chosen, values of skill and precision are taken from the class CustomAILevel.
         levelAI = 3;
@@ -23,7 +23,7 @@ class CfgDifficultyPresets {
             enemyTags = 0;      // Enemy name tags (0 = never, 1 = limited distance, 2 = always)
             detectedMines = 0;    // Detected mines (0 = never, 1 = limited distance, 2 = always)
             commands = 0;      // Commands (0 = never, 1 = fade out, 2 = always)
-            waypoints = 2;      // Waypoints (0 = never, 1 = fade out, 2 = always)
+            waypoints = 0;      // Waypoints (0 = never, 1 = fade out, 2 = always)
 
             /* Personal awareness */
             weaponInfo = 1;      // Weapon info (0 = never, 1 = fade out, 2 = always)
