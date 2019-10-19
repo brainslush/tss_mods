8a45582a5f28efa1cb0b01f8a14d6b0d186f2a73
brainslush <benny@horrorservice.de>
2019-10-19 21:43:09 +0200

    Added Loadout config for Niarms

--- optionals/niarms/CfgTSSLoadouts.hpp
+++ optionals/niarms/CfgTSSLoadouts.hpp
@@ -2,7 +2,7 @@ class CfgTSSLoadouts {
     class CfgPrimaries {
         class rifle {
             class all {
-                class niarms {
+                class COMPONENT {
                     primaries[] = {
                         "hlc_rifle_RU556",
                         "hlc_rifle_RU5562",
@@ -26,9 +26,9 @@ class CfgTSSLoadouts {
                     defaultMagazine[] = {"hlc_30rnd_556x45_SOST_EMAG"};
                 };
             };
-            class des {
-                class niarms {
-                    primaries[] = {
+            class des: all {
+                class COMPONENT: COMPONENT {
+                    primaries[] += {
                         "hlc_rifle_416D10_tan",
                         "hlc_rifle_416D145_tan",
                         "hlc_rifle_416D165_tan"
@@ -36,9 +36,12 @@ class CfgTSSLoadouts {
                     defaultMagazine[] = {"hlc_30rnd_556x45_SOST_EMAG"};
                 };
             };
-            class wdl {
-                class niarms {
-                    primaries[] = {
+            class jgl: all {};
+            class mdt: all {};
+            class snw: all {};
+            class wdl: all {
+                class COMPONENT: COMPONENT {
+                    primaries[] += {
                         "hlc_rifle_416D10_wdl",
                         "hlc_rifle_416D145_wdl",
                         "hlc_rifle_416D165_wdl"
@@ -49,7 +52,7 @@ class CfgTSSLoadouts {
         };
         class riflegl {
             class all {
-                class niarms {
+                class COMPONENT {
                     primaries[] = {
                         "hlc_rifle_416D10_gl",
                         "hlc_rifle_416D145_gl",
@@ -59,10 +62,15 @@ class CfgTSSLoadouts {
                     defaultMagazine[] = {"hlc_30rnd_556x45_SOST_EMAG", "1Rnd_HE_Grenade_shell"};
                 };
             };
+            class des: all {};
+            class jgl: all {};
+            class mdt: all {};
+            class snw: all {};
+            class wdl: all {};
         };
         class lmg {
             class all {
-                class niarms {
+                class COMPONENT {
                     primaries[] = {
                         "hlc_m249_SQuantoon",
                         "hlc_lmg_mk46",
@@ -71,18 +79,30 @@ class CfgTSSLoadouts {
                     defaultMagazine[] = {"hlc_200rnd_556x45_M_SAW"};
                 };
             };
+            class des: all {};
+            class jgl: all {};
+            class mdt: all {};
+            class snw: all {};
+            class wdl: all {};
         };
         class ar {
             class all {
-                class niarms {
-                    primaries[] = {"hlc_rifle_M27IAR"};
+                class COMPONENT {
+                    primaries[] = {
+                        "hlc_rifle_M27IAR"
+                    };
                     defaultMagazine[] = {"hlc_50rnd_556x45_EPR"};
                 };
             };
+            class des: all {};
+            class jgl: all {};
+            class mdt: all {};
+            class snw: all {};
+            class wdl: all {};
         };
         class mmg {
             class all {
-                class niarms {
+                class COMPONENT {
                     primaries[] = {
                         "hlc_lmg_mk48mod1",
                         "hlc_lmg_mk48"
@@ -90,14 +110,27 @@ class CfgTSSLoadouts {
                     defaultMagazine[] = {"hlc_200Rnd_762x51_Barrier_M60E4"};
                 };
             };
+            class des: all {};
+            class jgl: all {};
+            class mdt: all {};
+            class snw: all {};
+            class wdl: all {};
         };
         class dmr {
             class all {
-                class niarms {
-                    primaries[] = {"hlc_rifle_m14sopmod", "hlc_rifle_M14dmr_Rail"};
+                class COMPONENT {
+                    primaries[] = {
+                        "hlc_rifle_m14sopmod",
+                        "hlc_rifle_M14dmr_Rail"
+                    };
                     defaultMagazine[] = {"hlc_20Rnd_762x51_mk316_M14"};
                 };
             };
+            class des: all {};
+            class jgl: all {};
+            class mdt: all {};
+            class snw: all {};
+            class wdl: all {};
         };
     };
 };
